833ce59f1c39e3e293089a7cbe7fa8ad5735f74f
Kim Lidström <kim@dxtr.im>
2013-11-09 01:49:15 +0100

    Updated the init.sh file

--- init.sh
+++ init.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 
+# This should be run before the first time vim is run with this configuration.
+
+if [[ -d $HOME/.vim ]]; then
+	echo "You already have a .vim directory!"
+	exit
+fi
+
 git submodule init
 git submodule update
 cd ..
-mv vim ~/.vim
+mv vim $HOME/.vim
 ln -s ~/.vim/vimrc ~/.vimrc
-mkdir ~/.vim/backup
-mkdir ~/.vim/swap
+
+if [[ ! -d $HOME/.vim/bundle ]]; then
+	mkdir $HOME/.vim/bundle
+fi
+
+if [[ ! -d $HOME/.vim/backup ]]; then
+	mkdir $HOME/.vim/backup
+fi
+
+if [[ ! -d $HOME/.vim/swap ]]; then
+	mkdir $HOME/.vim/swap
+fi
+
+vim -c BundleInstall
